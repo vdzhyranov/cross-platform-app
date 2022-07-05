da9de48e5fdba3be1962cee76f548c35f2634cf9
vDzhyranov <v.dzhyranov@gmail.com>
2022-07-05 22:31:10 +0300

    add graphql playground

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,25 @@
 const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
+
+const app = express()
+const port = process.env.PORT || 4000
+
+const typeDefs = gql `
+  type Query {
+      hello: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => 'hello'
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.applyMiddleware({app, path: '/api'})
+
+app.get('/', (req, res) => res.send('hello world'))
+
+app.listen({ port }, () => console.log(`server running at localhost:${port}${server.graphqlPath}`))
